20d192fbec3e314dbb31414c75db839467c3f624
Elena Slyusareva <sportgirl55@bk.ru>
2021-01-31 02:35:48 +0500

    Initial commit. Task 13

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const Header = () => {
+  return <header className="header">
+    <img src="https://wallpapertag.com/wallpaper/full/9/7/1/633568-vertical-shut-up-wallpaper-1920x1080-ipad.jpg" alt="" />
+  </header>
+}
+
+export default Header;
